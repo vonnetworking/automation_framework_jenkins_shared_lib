751cf332a705ff474287db67459eb28ff3fe4668
Av2 <vonnetworking2@gmail.com>
2021-03-07 16:49:20 -0500

    updated shared lib to actually run the thing

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -4,7 +4,25 @@ def call() {
 
     println("------########---------")
     println(env);
-    
+
     println("------########---------")
-    sh "printenv"
+    sh '''
+        echo "hello World - ${GIT_URL}.${GIT_COMMIT}"
+
+        cat <<EOT >greetings.txt
+        [url "git@github.com:"]
+        insteadOf = https://github.com/
+        
+        EOT
+
+        ansible-galaxy install --roles-path="${WORKSPACE}/roles" git+${GIT_URL} --force
+        ansible-galaxy install --roles-path="${WORKSPACE}/roles" -r requirements.yml
+
+        ansible-playbook -i 127.0.0.1, wrapper.yml --connection=local \
+        -e "role_name=\"${WORKSPACE}\"" 
+
+        # ls roles/
+        # env
+    '''
+
 }
